fc3806f5587c6c19c1878071a63e0d891e6b3eb2
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2023-08-15 10:54:15 +0800

    Add new playRound funtion and Test it
    I add a new playRound function to rock-paper-scissors.js and the
    parameter `playerSelection` is case-insensitive. Also, I have tested
    this function many times.

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -4,3 +4,30 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 // console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats paper';
+    } else {
+        return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+    }
+}
+
+let playerSelection = prompt("Choose one of rock, paper and scissor", "rock");
+playerSelection = playerSelection.toLowerCase();
+if (playerSelection === null) {
+    alert("Invalid input");
+}
+if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+} else {
+    alert("Invalid choice");
+}
